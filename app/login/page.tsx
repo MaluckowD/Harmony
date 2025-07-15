9c4d0435ccf597e0c0cc7dfe610a70c34639cf1a
Mitya <dim.maluckow2017@yandex.ru>
2025-07-15 22:29:46 +0300

    install shadcn library and create ui for login page

---
+++ app/login/page.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import type React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+import { Separator } from "@/components/ui/separator"
+import { Music } from "lucide-react"
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background px-4">
+        <Card className="w-full max-w-sm m-0 mx-auto">
+            <CardHeader className="text-center">
+                <div className="flex justify-center mb-4">
+                    <div className="p-3 bg-primary rounded-full">
+                    <Music className="h-8 w-8 text-primary-foreground" />
+                    </div>
+                </div>
+                <CardTitle className="text-2xl">Добро пожаловать</CardTitle>
+                <CardDescription>
+                Войдите в свой аккаунт для доступа к музыке
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                <div className="flex flex-col gap-6">
+                    <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                    />
+                    </div>
+                    <div className="grid gap-2">
+                    <div className="flex items-center">
+                        <Label htmlFor="password">Пароль</Label>
+                    </div>
+                    <Input id="password" type="password" required />
+                    </div>
+                    <Button type="submit" className="w-full">
+                        Войти
+                    </Button>
+                </div>
+                </form>
+
+                <Separator className="my-6" />
+
+                <div className="text-center">
+                    <p className="text-sm text-muted-foreground">
+                    Нет аккаунта?{" "}
+                    <Link href="/register" className="text-primary hover:underline">
+                        Зарегистрироваться
+                    </Link>
+                    </p>
+                </div>
+            </CardContent>
+        </Card>
+    </div>
+  )
+}
